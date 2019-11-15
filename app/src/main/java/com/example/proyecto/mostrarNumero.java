09d8d2eba8c78f432c7147d931cca4aa9f9467dd
aldayalar390 <aldayalar24@gmail.com>
2019-11-15 10:12:35 -0600

    actualizando la java class mostrar numero

--- app/src/main/java/com/example/proyecto/mostrarNumero.java
+++ app/src/main/java/com/example/proyecto/mostrarNumero.java
@@ -1,4 +1,15 @@
 package com.example.proyecto;
 
-public class mostrarNumero {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mostrarNumero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_numero);
+
+    }
 }
