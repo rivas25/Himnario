7c838d6887cadb52f1f9339b5f56a457686cc867
esthefany933 <teffamorales98@gmail.com>
2019-11-15 09:30:30 -0600

    Subiendo la activity mostrar autor y la clase mostrar autor

---
+++ app/src/main/java/com/example/proyecto/MostrarAutor.java
@@ -0,0 +1,14 @@
+package com.example.proyecto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MostrarAutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_autor);
+    }
+}
