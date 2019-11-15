637b760ddd14b0f251d53de5956e9db10c483f45
rivas25 <rivasyk05@gmail.com>
2019-11-15 09:39:29 -0600

    cree la Java class Config

--- app/src/main/java/com/example/proyecto/MantenimientoMySQL.java
+++ app/src/main/java/com/example/proyecto/MantenimientoMySQL.java
@@ -1,8 +1,86 @@
 package com.example.proyecto;
 
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MantenimientoMySQL {
 
+    Dto datos = new Dto();
 
+    boolean estadoGuardar = false;
+    boolean estadoEliminar = false;
+
+    private ProgressDialog pd;
+    AlertDialog.Builder dialogo1;
+    AlertDialog.Builder dialogo;
+    ProgressDialog progressDialog;
+
+    //List<Productos> productosList;
+    //List<> productosList;
+    ProductsAdapter adapter;
+
+    public void guardar(final Context context, final String codigo, final String letra, final String genero , final String autor, final String nombre){
+        String url = Config.urlGuardar;
+        //String url = "localhost/democrudsis21a/guardar.php";
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //En este método se recibe la respuesta en json desde el web service o API.
+
+                        try{
+                            JSONObject requestJSON = new JSONObject(response.toString());
+                            String estado = requestJSON.getString("estado");
+                            String mensaje = requestJSON.getString("mensaje");
+
+                            if(estado.equals("1")){
+                                Toast.makeText(context, mensaje, Toast.LENGTH_SHORT).show();
+                                //Toast.makeText(context, "Registro almacenado en MySQL.", Toast.LENGTH_SHORT).show();
+                            }else if(estado.equals("2")){
+                                Toast.makeText(context, ""+mensaje, Toast.LENGTH_SHORT).show();
+                            }
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                            //Toast.makeText(context, "Se encontrarón problemas...", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //En este método se notifica al usuario acerca de un posible error al tratar de
+                //realizar una acción cualquier en la base de datos remota.
+                Toast.makeText(context, "No se puedo guardar. \n" +
+                        "Verifique su acceso a internet.", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //En este método se colocan o se setean los valores a recibir por el fichero *.php
+                Map<String, String> map = new HashMap<>();
+                map.put("Content-Type", "application/json; charset=utf-8");
+                map.put("Accept", "application/json");
+                map.put("codigo", codigo);
+                map.put("letra", letra);
+                map.put("genero",genero );
+                map.put("nombre",nombre);
+                map.put("autor",autor );
+                return map;
+            }
+        };
+
+        MySingleton.getInstance(context).addToRequestQueue(request);
+
+    }
 
 }
