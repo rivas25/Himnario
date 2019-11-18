ddc426c514f4595e94fcb725630abc1e9f5a8cd2
aldayalar390 <aldayalar24@gmail.com>
2019-11-18 07:53:40 -0600

    se agrega java class alabanzas

---
+++ app/src/main/java/com/example/proyecto/Alabanzas.java
@@ -0,0 +1,17 @@
+package com.example.proyect;
+
+public class Alabanzas {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String letra;
+
+    public Alabanzas() {
+    }
+
+    public Alabanzas(int id, String titulo, String autor, String letra) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.letra = letra;
+    }
