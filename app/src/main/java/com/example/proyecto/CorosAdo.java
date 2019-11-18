264b09e33cb4cd8f1c74b8505fde7a7c18e77b04
aldayalar390 <aldayalar24@gmail.com>
2019-11-18 08:07:56 -0600

    adicione la java claas CorsAdo

---
+++ app/src/main/java/com/example/proyecto/CorosAdo.java
@@ -0,0 +1,17 @@
+package com.example.proyecto;
+
+public class CorosAdo {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String letra;
+
+    public CorosAdo() {
+    }
+
+    public CorosAdo(int id, String titulo, String autor, String letra) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.letra = letra;
+    }
