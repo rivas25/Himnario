fe9bc4d68c48b098c2f0e9debc469c787294cf37
aldayalar390 <aldayalar24@gmail.com>
2019-11-18 08:10:16 -0600

    codigo final de java class CorosAdo

--- app/src/main/java/com/example/proyecto/CorosAdo.java
+++ app/src/main/java/com/example/proyecto/CorosAdo.java
@@ -15,3 +15,48 @@ public class CorosAdo {
         this.autor = autor;
         this.letra = letra;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+
+    @Override
+    public String toString() {
+        return  titulo;
+    }
+
+    public  String tostring(){
+        return  "ID: " + id + "\n"+
+                "TÍTULO: " + titulo + "\n"+
+                "AUTOR: " + autor + "\n\n"+
+                "LETRA: " +  "\n" + letra ;
+    }
+}
