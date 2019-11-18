7a461ccb028e0d6f6b73f58e61efc2215213b8c8
aldayalar390 <aldayalar24@gmail.com>
2019-11-18 07:56:04 -0600

    adicione la parte final del codigo de la java class alabanzas

--- app/src/main/java/com/example/proyecto/Alabanzas.java
+++ app/src/main/java/com/example/proyecto/Alabanzas.java
@@ -1,4 +1,4 @@
-package com.example.proyect;
+package com.example.proyecto;
 
 public class Alabanzas {
     private int id;
@@ -15,3 +15,48 @@ public class Alabanzas {
         this.autor = autor;
         this.letra = letra;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+
+    @Override
+    public String toString() {
+        return id +  " ~ " + titulo;
+    }
+
+    public  String tostring(){
+        return  "ID: " + id + "\n"+
+                "TÍTULO: " + titulo + "\n"+
+                "AUTOR: " + autor + "\n\n"+
+                "LETRA: " +  "\n" + letra ;
+    }
+}
+
--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.proyect;
+package com.example.proyecto;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
