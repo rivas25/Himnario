ffa75b7862ac4eaa8c58d3c17b4c64d278b41bcd
aldayalar390 <aldayalar24@gmail.com>
2019-11-18 08:46:21 -0600

    adicione las tres clases que van a controlar el envio y recibir datos

---
+++ app/src/main/java/com/example/himnario/Alabanzas.java
@@ -0,0 +1,62 @@
+package com.example.himnario;
+
+public class Alabanzas {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String letra;
+
+    public Alabanzas() {
+    }
+
+    public Alabanzas(int id, String titulo, String autor, String letra) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.letra = letra;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+
+    @Override
+    public String toString() {
+        return id +  " ~ " + titulo;
+    }
+
+    public  String tostring(){
+        return  "ID: " + id + "\n"+
+                "TÍTULO: " + titulo + "\n"+
+                "AUTOR: " + autor + "\n\n"+
+                "LETRA: " +  "\n" + letra ;
+    }
+}
